8114b0c52f9908b63802a8f13ae150ea5f96ad35
Christian Thompson <wynand1004@gmail.com>
2024-03-31 07:25:14 +0900

    DemoGame Updated
    Added multiple enemies
    Added new explosion.wav sound effect

--- DemoGame.java
+++ DemoGame.java
@@ -7,7 +7,7 @@ class DemoGame
    {
       Game game = new Game("Side Scrolling Shooter Demo");
       game.setBackgroundColor(Color.BLACK);
-      game.setFPS(60);
+      game.setFPS(30);
             
       // Create Sprites
       Sprite player = new Sprite(0, 400, "player.png");
@@ -17,12 +17,17 @@ class DemoGame
       missile.setDX(200);
       game.addSprite(missile);
       
-      Sprite enemy = new Sprite(1000.0, 400.0, "enemy.png");
-      enemy.setDX(-100);
-      game.addSprite(enemy);
+      for(int i=0;i<25;i++)
+      {
+        double x = (Math.random() * 1000) + 1024;
+        double y = (Math.random() * 768);
+        Sprite enemy = new Sprite(x, y, "enemy.png");
+        enemy.setDX(Math.random() * -200);
+        game.addSprite(enemy);
+      }
       
       // Create sounds
-      Sound beep = new Sound("beep.wav");
+      Sound explosion = new Sound("explosion.wav");
       
       // Main Game Loop
       while(true)
@@ -63,7 +68,16 @@ class DemoGame
               {
                   sprite.setX(1200);
                   missile.setX(12000);
-                  beep.play();
+                  explosion.play();
+              }
+              
+              // Move missiles back if off screen
+              if(sprite.getX() < -100)
+              {
+                 double x = (Math.random() * 1000) + 1024;
+                 double y = (Math.random() * 400);
+                 sprite.setX(x);
+                 sprite.setY(y);
               }
           }
       }
