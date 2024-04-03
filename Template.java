c1ebe8f63a5698c5be27e3ba2c5d5ccaeac56b5b
Christian Thompson <wynand1004@gmail.com>
2024-04-03 20:11:12 +0900

    Added Minimal Template File
    New games should start with this

---
+++ Template.java
@@ -0,0 +1,30 @@
+import java.awt.Color;
+import java.awt.event.KeyEvent;
+
+class Template
+{
+   // Main Method
+   public static void main(String[] args)
+   {
+      SJGL game = new SJGL("Template");
+      game.setBackgroundColor(Color.BLACK);
+            
+      // Create Sprites
+      // Sprite spriteName = new Sprite(x, y, "image.png");
+      // game.addSprite(spriteName);
+      
+      // Create sounds
+      // Sound explosion = new Sound("sound.wav");
+      
+      // Create labels
+      // Label labelName = new Label("text", x, y);
+      // game.addLabel(labelName);
+      
+      // Main Game Loop
+      while(true)
+      {
+          // Your Game Code Goes Here
+          
+      }
+   }
+}
